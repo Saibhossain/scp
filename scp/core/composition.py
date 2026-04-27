78c5f0913fe4777a738bcd4dd33e33ed7c8b9d14
mdsaibhossain <saibhossain5@gmail.com>
2026-04-27 12:30:29 +0600

    SignedContext, SignedInvocation, ExecutionState

--- scp/core/composition.py
+++
@@ -1,7 +0,0 @@
-from scp.core.policy import C_LOW, C_HIGH
-
-
-def compose_labels(labels):
-    if C_LOW in labels:
-        return C_LOW
-    return C_HIGH
--- scp/core/state.py
+++
@@ -1,4 +0,0 @@
-class ExecutionState:
-    def __init__(self):
-        self.seq = 0
-        self.contexts = {}
