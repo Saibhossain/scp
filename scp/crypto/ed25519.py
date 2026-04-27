1c4fa4ea0b0259a2fd9a221726cc0f1333854951
mdsaibhossain <saibhossain5@gmail.com>
2026-04-27 12:33:05 +0600

    Real signature scheme

--- scp/crypto/ed25519.py
+++ scp/crypto/ed25519.py
@@ -1,34 +1,21 @@
-from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PrivateKey, Ed25519PublicKey
-from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PrivateKey
 
 
 class Ed25519Signer:
-    def __init__(self, private_key: Ed25519PrivateKey):
-        self.sk = private_key
-        self.pk = private_key.public_key()
+    def __init__(self, sk):
+        self.sk = sk
+        self.pk = sk.public_key()
 
     @staticmethod
     def generate():
-        sk = Ed25519PrivateKey.generate()
-        return Ed25519Signer(sk)
+        return Ed25519Signer(Ed25519PrivateKey.generate())
 
-    def sign(self, message: bytes) -> bytes:
-        return self.sk.sign(message)
+    def sign(self, msg: bytes) -> bytes:
+        return self.sk.sign(msg)
 
-    def public_bytes(self) -> bytes:
-        return self.pk.public_bytes(
-            encoding=serialization.Encoding.Raw,
-            format=serialization.PublicFormat.Raw
-        )
-
-
-class Ed25519Verifier:
-    def __init__(self, public_key_bytes: bytes):
-        self.pk = Ed25519PublicKey.from_public_bytes(public_key_bytes)
-
-    def verify(self, message: bytes, signature: bytes) -> bool:
+    def verify(self, sig: bytes, msg: bytes) -> bool:
         try:
-            self.pk.verify(signature, message)
+            self.pk.verify(sig, msg)
             return True
-        except:
+        except Exception:
             return False
